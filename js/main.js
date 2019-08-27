d2f4e15dc73380cdb4962150b4c2005212859f81
Sariel <sarielh90@gmail.com>
2019-08-28 01:17:18 +0300

    s

--- js/main.js
+++ js/main.js
@@ -11,12 +11,21 @@ $(document).ready(function(){
         source: availableTags
     });
 
+
+
     $( "#ui-id-1" ).click(function() {
 
         if($('#tags').val() == 'Tel Aviv'){
-            alert("Tel Aviv is Here!")
+            // alert("Tel Aviv is Here!")
+            $('#MainWeatherWeeklyDay1DetailsCityNameDiv').text('Tel-Aviv');
+            $('#MainWeatherWeeklyDay1DetailsTempDiv').text('33°');
+            $('#WeatherDay2TempDiv').text('35°/24');
+            $('#WeatherDailyImage2Div').css("background-image", "url(./images/016-sun.png)");
+            $('#WeatherDay3TempDiv').text('37°/25');
+            $('#WeatherDay4TempDiv').text('29°/21');
+            $('#WeatherDay3TempDesc').text('Sunny');
         }
 
     });
 
-});
+    });
