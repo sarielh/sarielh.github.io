010621400bcdcd23bc430868031f3f89bfbc8eed
Sariel <sarielh90@gmail.com>
2019-08-29 13:10:00 +0300

    s

--- webhose/js/main.js
+++ webhose/js/main.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 $.getJSON('countries.json', function(data) {
     console.log(data);
 
-    var option_country;
+    var option_country = '';
 
     for (const [key, value] of Object.entries(data)) {
         console.log(key, value);
         option_country += '<option>'+ value +'</option>';
     }
 
-    $('SourceCountry').append(option_country);
+    $('#SourceCountry').append(option_country);
 
-}); 
+});
 
 // $('#MainDiv').append('<div>Sariel Hadas is here!</div>');
 
