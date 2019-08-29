60bd77684b4746545fdd7fc74f25c782c8b43582
Sariel <sarielh90@gmail.com>
2019-08-29 15:17:35 +0300

    דשגשדג
    sariel

--- webhose/js/main.js
+++ webhose/js/main.js
@@ -25,12 +25,31 @@ $("#SubmitBtn").click(function(){
     var source_country = $('#SourceCountry')[0].innerText;
     var reason = $('#Reason')[0].value;
 
-    var coverage_request = {Email : client_email, Domain: client_domain, SourceType: source_type, reason:reason};
+    var coverage_request = {Email : client_email, Domain: client_domain, SourceType: source_type, Reason:reason};
 
 
     all_coverage_request.push(coverage_request);
 
-    console.log(all_coverage_request);
+    // console.log(all_coverage_request);
+
+    var table_request = "<table><tr>";
+    table_request += "<th>Email</th>";
+    table_request += "<th>Domain</th>";
+    table_request += "<th>Source Type</th>";
+    table_request += "<th>Reason</th>";
+    table_request += "</tr><tr>";
+
+
+    for (const [key, value] of Object.entries(all_coverage_request)) {
+        console.log(key, value);
+        var value_test = value;
+        connsole.log(value_test);
+        table_request += "<td>'+ value +'</td>";
+    }
+
+    table_request += "</tr></table>";
+
+    $('#MainDiv').append(table_request);
 });
 
 
