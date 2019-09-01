5fd52d1aab460d11df136293d0322932e0e786cc
Sariel <sarielh90@gmail.com>
2019-09-01 15:17:13 +0300

    Sariel

--- webhose/js/main.js
+++ webhose/js/main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-$.getJSON('https://webhose.io/names.json.', function(data) {
+$.getJSON('https://webhose.io/names.json', function(data) {
     // console.log(data);
 
     var option_country = '';
